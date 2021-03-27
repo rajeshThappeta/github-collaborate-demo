91202512cfb61a7b4fa54ff80554790c51f81c71
rajesh.t7982 <rajesh.t7982@gmail.comm>
2021-03-27 16:51:36 +0530

    login added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactusComponent } from './contactus/contactus.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactusComponent
+    ContactusComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
