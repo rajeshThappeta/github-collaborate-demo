eb044f25c13a9f38ff64eb29702fd90d3b5f6254
rajeshasstprof <rajeshasstprof@gmail.com>
2021-03-27 16:17:22 +0530

    contactus added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactusComponent
   ],
   imports: [
     BrowserModule,
